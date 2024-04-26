d16a450889c5a142b8dde1ba408b22795ffa275f
Miguel Rivadavia <mrivadavia91@gmail.com>
2024-04-26 16:18:34 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+
+
+let precio = 2000
+let precioConDescuento = 10
+let tranferencia = precio/precioConDescuento
+let credito = precio
+
+if(precio){prompt("transferencia o credito")
+    alert("ingrese forma de pago");
+}
+else if(tranferencia){
+    
+    alert("tranferencia");
+}
+else if(credito){
+    alert(precio);
+}
+
