a5bbd215ea24f7a9bc44a0a6946645d9e4ee0502
Miguel Rivadavia <mrivadavia91@gmail.com>
2024-05-17 22:07:52 -0300

    modificado

--- main.js
+++ main.js
@@ -1,24 +1,74 @@
-function pagar(){
-alert("el total es 600")
-let precio = 600
-let consulta = prompt("en cuantas cuotas abona? 1,3 o 6")
 
-if(consulta){
+const Producto = function(nombre,precio,stock){
+    this.nombre= nombre,
+    this.precio = precio
+    this. stock = stock
+}
+                   
+let producto1= new Producto ("anillo", 100, 20)
+let producto2= new Producto ("arito", 150, 26)
+let producto3= new Producto ("pulsera", 500, 45)
+let producto4= new Producto ("difusor", 1000, 23)
 
-    switch(consulta){
-        case "1":
-            alert("esta bien son 600")
-            break
-        case "3":
-            alert("cada cuota queda en 200") 
-            break
-        case "6":
-            alert("cada cuota queda en 100")   
+let lista = [producto1,producto2,producto3,producto4]
+
+
+
+alert("ingrese la opcion del producto a comprar, para salir ingrese 0")
+let seleccionarProductos = Number(prompt("1-anillo $100 2-arito $150 3-pulsera $500 4-difusor $1000"))
+let seleccionarCantidad
+let total = 0
+const cantidad = (cant, precio) =>{
+    return cant * precio
+}
+
+let productos = []
+
+
+while(seleccionarProductos !=0){
+    switch (seleccionarProductos){
+        case 1:
+            seleccionarCantidad = Number(prompt("el producto seleccionado es anillo, ingresa cantidad"))
+            productos.push("anillo  " + seleccionarCantidad)
+            total += cantidad(seleccionarCantidad,100)
+        break;
+        case 2:
+            seleccionarCantidad = Number(prompt("el producto seleccionado es arito, ingresa cantidad"))
+            productos.push("arito  " + seleccionarCantidad)
+            total += cantidad(seleccionarCantidad,150)
+        break;
+        case 3:
+            seleccionarCantidad = Number(prompt("el producto seleccionado es pulsera, ingresa cantidad"))
+            productos.push("pulsera  " + seleccionarCantidad)
+            total += cantidad(seleccionarCantidad,500)
+        break;
+        case 4:
+            seleccionarCantidad = Number(prompt("el producto seleccionado es difusor, ingresa cantidad"))
+            productos.push("difusor  " + seleccionarCantidad)
+            total += cantidad(seleccionarCantidad,1000)
+        break;
+
 
     }
-    alert("muchas gracias")
+    seleccionarProductos = Number(prompt("1-anillo $100 2-arito $150 3-pulsera $500 4-difusor $1000"))
 }
 
-}
 
-pagar()
+
+
+alert("el total de la compra es  "  + total)
+
+console.log(productos)
+
+function encontrar (){
+    
+    let encontrado = lista.filter((x)=>x.seleccionarCantidad)
+
+    if(encontrado.length >=2){
+        console.log(encontrado)}
+    else{
+        console.log(encontrado + "compro por menor")
+    }
+
+ 
+}
